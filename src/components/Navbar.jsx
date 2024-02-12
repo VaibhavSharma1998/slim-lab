84b19ca7c357e228dfeafdb2c315494d7f1fdf9b
VaibhavSharma1998 <sharmavaibhav203@gmail.com>
2024-02-12 20:45:20 +0530

    "today complete this website"

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,34 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import logo from "../images/logo.png";
-import hamberger from "../images/hamberger.png"
+import hamberger from "../images/hamberger.png";
+import { AiOutlineClose } from "react-icons/ai";
 
-import './styles.css'
+import "./styles.css";
 
 export const Navbar = () => {
-  
+  const [toggle, setToggle] = useState(false);
+  const handleToggleClick = () => {
+    setToggle(!toggle);
+  };
+
   return (
     <div>
       <section>
         <div className="container">
           <navbar className="navbar-flex">
-            <img src={hamberger} alt="hamberger" className="hamberger"/>
+            <img
+              src={hamberger}
+              alt="hamberger"
+              className="hamberger"
+              onClick={handleToggleClick}
+            />
             <img src={logo} alt="logo" className="logo" />
+            {/* {toggle && (
+              <AiOutlineClose
+                className="close-icon"
+                onClick={handleToggleClick}
+              />
+            )} */}
             <button type="button" className="navbar-btn">
               DO I QUALIFY?
             </button>
